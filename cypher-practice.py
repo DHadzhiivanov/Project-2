f9cabaca34e58e2789e28bfec7f6677ed7694bb4
DHadzhiivanov <570287@student.fontys.nl>
2025-09-30 11:23:31 +0200

    tried adding x after every first duplicated letter but failed and it adds x after every letter

--- cypher-practice.py
+++ cypher-practice.py
@@ -1,4 +1,11 @@
 #every 2 letters a space will be added.
 plaintext = input("enter word: ")
-spaced_word = " ".join([plaintext[i:i+2] for i in range(0, len(plaintext), 2)])
+
+for i in range(len(plaintext) - 1):
+	if plaintext[i] == plaintext[i+1]:
+		append_filler = "x".join([plaintext[i:i+1] for i in range(0,len(plaintext),1)])
+
+spaced_word = " ".join([append_filler[i:i+2] for i in range(0, len(append_filler), 2)])
+
+
 print(spaced_word)
