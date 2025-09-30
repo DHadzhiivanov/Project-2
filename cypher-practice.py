e348a669f3d5bb4422c230bdfd3ef511eb03cc30
DHadzhiivanov <570287@student.fontys.nl>
2025-09-30 13:49:23 +0200

    formatting changes

--- cypher-practice.py
+++ cypher-practice.py
@@ -1,23 +1,23 @@
-#every 2 letters a space will be added.
+# every 2 letters a space will be added.
 plaintext = input("enter word: ").lower()
 
-i=0
-prepared=""
+i = 0
+prepared = ""
 
-while i<len(plaintext):
-	prepared += plaintext[i]
-	if i + 1 < len(plaintext):
-		if plaintext[i] == plaintext[i+1]:
-			prepared += "x"
-			i+=1
-		else:
-			prepared+=plaintext[i+1]
-			i+=2
-	else:
-		i+=1
+while i < len(plaintext):
+    prepared += plaintext[i]
+    if i + 1 < len(plaintext):
+        if plaintext[i] == plaintext[i+1]:
+            prepared += "x"
+            i += 1
+        else:
+            prepared += plaintext[i+1]
+            i += 2
+    else:
+        i += 1
 
 if len(prepared) % 2 != 0:
-	prepared += "x"
+    prepared += "x"
 
 spaced_word = " ".join([prepared[i:i+2] for i in range(0, len(prepared), 2)])
 
