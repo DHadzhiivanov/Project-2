c684299b9d87011453e8cada099ecdcf0b803c6e
DHadzhiivanov <570287@student.fontys.nl>
2025-09-30 11:32:55 +0200

    fixed the adding of x after letter and now only adds it properly but does it only once, so if a word like ballloon appears it will have a duplicate again and x wont be placed again

--- cypher-practice.py
+++ cypher-practice.py
@@ -1,11 +1,15 @@
 #every 2 letters a space will be added.
 plaintext = input("enter word: ")
 
-for i in range(len(plaintext) - 1):
-	if plaintext[i] == plaintext[i+1]:
-		append_filler = "x".join([plaintext[i:i+1] for i in range(0,len(plaintext),1)])
+chars = list(plaintext)
 
-spaced_word = " ".join([append_filler[i:i+2] for i in range(0, len(append_filler), 2)])
+for i in range(len(chars) - 1):
+	if chars[i] == chars[i+1]:
+		chars.insert(i+1,'x')
+		break
 
+modified = "".join(chars)
+
+spaced_word = " ".join([modified[i:i+2] for i in range(0, len(modified), 2)])
 
 print(spaced_word)
