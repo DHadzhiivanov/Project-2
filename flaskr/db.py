00b6bb118a58e447c25551d4431666cd2f34a01b
DHadzhiivanov <570287@student.fontys.nl>
2025-10-20 15:37:47 +0200

    Creating the database

---
+++ flaskr/db.py
@@ -0,0 +1,37 @@
+import sqlite3
+from datetime import datetime
+
+import click
+from flask import current_app, g
+# g is a special object that is unique for each request.
+# It is used to store data that might be accessed by multiple functions during the request.
+# The connection is stored and reused instead of creating a new connection if get_db is called a second time in the same request.
+
+# current_app is another special object that points to the Flask application handling the request.
+# Since we used an application factory, there is no application object when writing the rest of our code.
+# get_db will be called when the application has been created and is handling a request, so current_app can be used.
+
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+    return g.db
+
+    # sqlite3.connect() establishes a connection to the file pointed at by the DATABASE configuration key.
+
+    # sqlite3.Row tells the connection to return rows that behave like dicts.
+    # This allows accessing the columns by name.
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
+
+    # close_db checks if a connection was created by checking if g.db was set.
+    # If the connection exists, it is closed.
+    # Further down we will tell our application about the close_db function in the application factory so that it is called after each request.
