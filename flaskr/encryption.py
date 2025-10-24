5e036add0138240227d38311bf80e69f3b447467
DHadzhiivanov <570287@student.fontys.nl>
2025-10-24 11:43:38 +0200

    added AES GCM encryption and UI changes

--- flaskr/encryption.py
+++ flaskr/encryption.py
@@ -1,29 +1,41 @@
-import random
-import string
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from cryptography.exceptions import InvalidTag
+from base64 import urlsafe_b64encode, urlsafe_b64decode
+import os
 
 
-def generate_key():
-    chars = string.ascii_letters + string.digits + string.punctuation + ' '
-    chars = list(chars)
-    random.shuffle(chars)
-    return ''.join(chars)
+def derive_key(password: str, salt: bytes, iterations: int = 200_000) -> bytes:
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=iterations,
+    )
+    return kdf.derive(password.encode('utf-8'))
 
 
-def encrypt_message(message, key):
-    encrypted = ""
-    for char in message:
-        if char in key:
-            encrypted += key[(key.index(char) + 5) % len(key)]
-        else:
-            encrypted += char
-    return encrypted
+def encrypt_message(plaintext: str, password: str) -> str:
+    salt = os.urandom(16)
+    key = derive_key(password, salt)
+    aesgcm = AESGCM(key)
+    nonce = os.urandom(12)
+    ciphertext = aesgcm.encrypt(nonce, plaintext.encode('utf-8'), None)
+    return urlsafe_b64encode(salt + nonce + ciphertext).decode('utf-8')
 
 
-def decrypt_message(message, key):
-    decrypted = ""
-    for char in message:
-        if char in key:
-            decrypted += key[(key.index(char) - 5) % len(key)]
-        else:
-            decrypted += char
-    return decrypted
+def decrypt_message(token_b64: str, password: str) -> str:
+    token = urlsafe_b64decode(token_b64.encode('utf-8'))
+    if len(token) < 16 + 12:
+        raise ValueError("Invalid token")
+    salt = token[:16]
+    nonce = token[16:28]
+    ciphertext = token[28:]
+    key = derive_key(password, salt)
+    aesgcm = AESGCM(key)
+    try:
+        pt = aesgcm.decrypt(nonce, ciphertext, None)
+    except InvalidTag:
+        raise
+    return pt.decode('utf-8')
